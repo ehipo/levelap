c1fdd2abcd180aa271fd4ce553535e0b389d3a5d
ehipo <efrain.hipo@provedatos.com>
2013-10-03 17:33:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Levelap::Application.config.secret_key_base = '7ce82908d226279414a87a4c9b927dfd8f233a5370d3225b3385106936508214bdf8980bc71bf09c756593d554b34629463f91d105c6edb14e4d2f752424a311'
